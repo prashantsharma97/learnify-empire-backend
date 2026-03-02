e2ec47dc3c1f95bde2a967c5499d69ac05070108
prashant <prashantatul565@gmail.co>
2026-03-02 18:08:18 +0530

    SS

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const connectDB = require('./config/db');
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);
 
